373fb22b0d2a0e001825838df7326772bbe2ce7e
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-08-08 00:33:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var Classes = Object.create(null);
  * @public 
  */ 
 exports.createConnection = function createConnection(config) { 
-  var Connection       = loadClass('Connection'); 
+  var Connection       = loadClass('Connection');  
   var ConnectionConfig = loadClass('ConnectionConfig');
 
   return new Connection({config: new ConnectionConfig(config)});
